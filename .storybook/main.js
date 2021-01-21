4cb4b1ce363d878d926d6af9096048394979fa6e
o0plamen <pmitev@altagram.com>
2021-01-21 10:45:00 +0100

    add build for storybooks

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,11 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  // changes the load order of our stories. First loads the Intro page
+  // automatically import all files ending in *.stories.js|mdx
+  stories: ['../src/components/Intro.stories.mdx','../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-a11y',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
-  ]
-}
+};
